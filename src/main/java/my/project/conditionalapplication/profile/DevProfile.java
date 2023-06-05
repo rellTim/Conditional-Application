c136ba61726343d6daba3f0a73256b34e8f68dbf
Tim <book9902@yandex.ru>
2023-06-05 19:29:11 +0300

    Initial commit annotation

--- src/main/java/my/project/conditionalapplication/profile/DevProfile.java
+++ src/main/java/my/project/conditionalapplication/profile/DevProfile.java
@@ -1,5 +1,10 @@
 package my.project.conditionalapplication.profile;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(value = "my.profile.dev", havingValue = "true")
 public class DevProfile implements SystemProfile{
     @Override
     public String getProfile() {
